f0350410ba36c210ea86a77c29db7f2e8714f6b4
fpbrtbbldb1t8g6e7z0zf495-admin <jaw071@student.usc.edu.au>
2024-10-30 17:31:32 +1000

    added comments to file and debug print statements to bugfix

--- Assets/GameObjectLoader.cs
+++ Assets/GameObjectLoader.cs
@@ -5,34 +5,53 @@ using UnityEngine;
 
 namespace LoadNamespace
 {
+    // Serializable class for loading GameObject data
     [System.Serializable]
     public class SerializableGameObject
     {
-        public string name;
-        public Vector3 position;
-        public Quaternion rotation;
+        public string name;           // Name of the GameObject
+        public Vector3 position;      // Position of the GameObject
+        public Quaternion rotation;   // Rotation of the GameObject
     }
 
+    // Wrapper class for deserialization
     [System.Serializable]
     public class SerializationWrapper<T>
     {
-        public List<T> items;
+        public List<T> items;    // List of serializable items
     }
 
     public class GameObjectLoader : MonoBehaviour
     {
         void Start()
         {
+            // Path to the JSON file
             string path = Application.dataPath + "/GameObjects.json";
+
+            // Read the JSON data from the file
             string json = File.ReadAllText(path);
 
+            // Deserialize the JSON data into a list of SerializableGameObjects
             SerializationWrapper<SerializableGameObject> wrapper = JsonUtility.FromJson<SerializationWrapper<SerializableGameObject>>(json);
 
+            // Print the list of items for debugging
+            Debug.Log("Loaded GameObjects:");
             foreach (SerializableGameObject data in wrapper.items)
             {
+                Debug.Log($"Name: {data.name}, Position: {data.position}, Rotation: {data.rotation}");
+            }
+
+            // Instantiate GameObjects in the scene
+            foreach (SerializableGameObject data in wrapper.items)
+            {
+                // Create a new GameObject with the saved name
                 GameObject newObj = new GameObject(data.name);
+
+                // Set the position and rotation of the new GameObject
                 newObj.transform.position = data.position;
                 newObj.transform.rotation = data.rotation;
+
+                // Additional properties can be applied here if needed
             }
         }
     }
